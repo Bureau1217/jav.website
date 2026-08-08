ae7d919be61c3fb01390ec1ab08f4c8526532841
azertypow <nico@villa1203.ch>
2026-08-08 22:01:48 +0200

    feat: init blocks page

---
+++ components/blocks.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="v-blocks"
+    >
+    </section>
+</template>
+
+
+
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+    message?: string
+}>()
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-blocks {
+}
+</style>

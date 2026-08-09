1258f900adc326ecded98747e5185f6253b469b1
azertypow <nico@villa1203.ch>
2026-08-09 11:20:58 +0200

    chore: block concert

---
+++ app/components/blocks/ConcertsEvent.global.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="v-block-concerts-event">
+    <!-- Point d'ancrage : contenu dynamique "concerts et événements" à implémenter -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-concerts-event {
+}
+</style>

21219e352541c83f867debcb794fcd187b5d7f97
azertypow <nico@villa1203.ch>
2026-08-09 11:22:05 +0200

    chore: block text

---
+++ app/components/blocks/Text.global.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="v-block-text" v-html="block.content.text" />
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-text {
+}
+</style>

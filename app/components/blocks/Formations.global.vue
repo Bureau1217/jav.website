908944acf44c5f5482fdad1d1c86f44b6a353163
azertypow <nico@villa1203.ch>
2026-08-09 11:21:14 +0200

    chore: block formation

---
+++ app/components/blocks/Formations.global.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="v-block-formations">
+    <!-- Point d'ancrage : contenu dynamique "formations" à implémenter -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-formations {
+}
+</style>

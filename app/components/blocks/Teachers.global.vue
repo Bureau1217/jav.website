ae9ba86abc505fa2c78298e7462cfe6abbb42bfa
azertypow <nico@villa1203.ch>
2026-08-09 11:21:50 +0200

    chore: block teachers

---
+++ app/components/blocks/Teachers.global.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="v-block-teachers">
+    <NuxtLink
+      v-for="teacher in block.teacherPages"
+      :key="teacher.id"
+      :to="`/${teacher.id}`"
+      class="v-block-teachers__item"
+    >
+      <img v-if="teacher.photo" :src="teacher.photo.url" :alt="teacher.photo.alt ?? teacher.title">
+      <span class="v-block-teachers__name">{{ teacher.title }}</span>
+      <span v-if="teacher.fonction" class="v-block-teachers__fonction">{{ teacher.fonction }}</span>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-teachers {
+}
+</style>

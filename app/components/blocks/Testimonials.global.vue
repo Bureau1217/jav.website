837a271ddd1977a79b740d281fd5cf01f3900c0a
azertypow <nico@villa1203.ch>
2026-08-09 11:21:59 +0200

    chore: block testimonials

---
+++ app/components/blocks/Testimonials.global.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="v-block-testimonials">
+    <!-- Point d'ancrage : contenu dynamique "citations alumni et étudiant·e·xs" à implémenter -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-testimonials {
+}
+</style>

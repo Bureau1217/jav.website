ef30392036a56ff655d5dcf172e33edc79e3dfba
azertypow <nico@villa1203.ch>
2026-08-09 11:22:10 +0200

    chore: block toggle

---
+++ app/components/blocks/Toggle.global.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="v-block-toggle">
+    <details
+      v-for="(item, index) in block.content.items"
+      :key="index"
+      class="v-block-toggle__item"
+    >
+      <summary>{{ item.titre }}</summary>
+      <div v-html="item.contenu" />
+    </details>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-toggle {
+}
+</style>

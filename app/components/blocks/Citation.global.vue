fbb6abb6e407023c22c585f6a3c490a8c3ee82ee
azertypow <nico@villa1203.ch>
2026-08-09 11:20:51 +0200

    chore: block citation

---
+++ app/components/blocks/Citation.global.vue
@@ -0,0 +1,19 @@
+<template>
+  <blockquote class="v-block-citation">
+    <div class="v-block-citation__content" v-html="block.content.contenu" />
+    <cite v-if="block.content.signature">{{ block.content.signature }}</cite>
+  </blockquote>
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-citation {
+}
+</style>

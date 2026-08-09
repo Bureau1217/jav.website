a9a2b7a8427856ddc61bf029dc95720b9b6879f8
azertypow <nico@villa1203.ch>
2026-08-09 11:21:03 +0200

    chore: block cta

---
+++ app/components/blocks/Cta.global.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="v-block-cta">
+    <div v-if="block.content.title" class="v-block-cta__title" v-html="block.content.title" />
+    <div
+      v-if="block.content.description"
+      class="v-block-cta__description"
+      v-html="block.content.description"
+    />
+    <a v-if="block.content.link" :href="block.content.link" class="v-block-cta__link">
+      {{ block.content.link }}
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-cta {
+}
+</style>

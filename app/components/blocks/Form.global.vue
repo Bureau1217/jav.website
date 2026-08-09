b986e54cfe55ef65a1055a7e3e7c1cb58cc29b17
azertypow <nico@villa1203.ch>
2026-08-09 11:21:08 +0200

    chore: block form

---
+++ app/components/blocks/Form.global.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="v-block-form" :data-form-type="block.content.form_type">
+    <!-- Point d'ancrage : formulaire "{{ block.content.form_type }}" à implémenter -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { KqlBlock } from '~~/shared/types/kql'
+
+defineProps<{
+  block: KqlBlock
+}>()
+</script>
+
+<style lang="scss" scoped>
+.v-block-form {
+}
+</style>
